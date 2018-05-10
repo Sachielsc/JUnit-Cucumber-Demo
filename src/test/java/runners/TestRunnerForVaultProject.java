dc30df8b3dd4a6b19a962681e4f55838b55a7f16
Charles Shu <cshu@planittesting.com>
2018-05-11 11:39:31 +1200

    1. add testRunner file for vault project 2. build the skeleton for "VaultSteps"

---
+++ src/test/java/runners/TestRunnerForVaultProject.java
@@ -0,0 +1,13 @@
+package runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/vaultFeatureFiles"
+        // glue= {"stepDefinitions"}
+)
+public class TestRunnerForVaultProject {
+}
---
+++ src/test/java/stepDefinitionsForVault/VaultSteps.java
@@ -0,0 +1,38 @@
+package stepDefinitionsForVault;
+
+import cucumber.api.PendingException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+import static org.junit.Assert.assertEquals;
+
+public class VaultSteps {
+    @Given("^I navigate to the Vault log in page$")
+    public void i_navigate_to_the_Vault_log_in_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I type in \"([^\"]*)\" as my user name$")
+    public void i_type_in_as_my_user_name(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I also type in \"([^\"]*)\" as my password$")
+    public void i_also_type_in_as_my_password(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should be able to see the Vault homepage$")
+    public void i_should_be_able_to_see_the_Vault_homepage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
