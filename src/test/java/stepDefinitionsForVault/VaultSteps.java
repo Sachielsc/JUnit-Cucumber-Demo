67475bf13426128bef62c72ee1023e917f74f108
Charles Shu <cshu@planittesting.com>
2018-05-11 13:34:13 +1200

    Finish building "VaultSteps", with page object model implemented
    TODO: modify the assertions

--- src/test/java/stepDefinitionsForVault/VaultSteps.java
+++ src/test/java/stepDefinitionsForVault/VaultSteps.java
@@ -1,38 +1,57 @@
 package stepDefinitionsForVault;
 
 import cucumber.api.PendingException;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.Before;
+import cucumber.api.java.After;
+import utils.Driver;
+import vaultProject.pageobjects.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class VaultSteps {
+
+    // variable definitions
+    private WebDriver driver;
+    private LoginPage loginPage;
+
+    @Before("@LoginSuccess")
+    public void init(){
+        driver = Driver.getWebDriver();
+        loginPage = new LoginPage(driver);
+    }
+
     @Given("^I navigate to the Vault log in page$")
     public void NavigateToVault() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        loginPage.NavigateToLoginPage();
     }
 
     @When("^I type in \"([^\"]*)\" as my user name$")
-    public void TypeUserName(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void TypeUserName(String userName) throws Throwable {
+        loginPage.TypeUserName(userName);
     }
 
     @When("^I also type in \"([^\"]*)\" as my password$")
-    public void TypePassWord(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void TypePassWord(String passWord) throws Throwable {
+        loginPage.TypePassword(passWord);
     }
 
+    @When("^I click the Login Button$")
+    public void ClickLoginButton() throws Throwable {
+        loginPage.clickLoginButton();
+    }
     @Then("^I should be able to see the Vault homepage$")
     public void AssertLogIn() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        String test = "tobedeleted";
+        assertEquals(test, "tobedeleted");
+    }
+
+    @After("@LoginSuccess")
+    public void tearDown(){
+        Driver.quitWebDriver();
     }
 }
--- src/test/resources/vaultFeatureFiles/VaultUser.feature
+++ src/test/resources/vaultFeatureFiles/VaultUser.feature
@@ -1,6 +1,10 @@
 Feature: Vault User Log in
+  Users need to login
+
+  @LoginSuccess
   Scenario: Log in successful
     Given I navigate to the Vault log in page
     When I type in "plan.6" as my user name
     And I also type in "plan01#" as my password
+    And I click the Login Button
     Then I should be able to see the Vault homepage
